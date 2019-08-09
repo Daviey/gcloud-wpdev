f6097df967cc152b76e74203b93fe3cebcfae96f
Dave Walker (Daviey) <email@daviey.com>
2019-08-09 22:26:27 +0100

    Added support for env variable for $DB_PASS
    Signed-off-by: Dave Walker (Daviey) <email@daviey.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ./cloud_sql_proxy -instances="$@"=tcp:3306 &
-dev_appserver.py --php_executable_path=/usr/local/bin/php-cgi --host 0.0.0.0 /opt/site/
+dev_appserver.py --php_executable_path=/usr/local/bin/php-cgi --host 0.0.0.0 --env_var DB_PASS="${DB_PASS}" /opt/site/app.yaml
